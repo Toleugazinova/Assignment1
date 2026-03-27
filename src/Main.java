abef4aac507b194c0a0bc8b4bbc54daea9dcfb07
Amina Toleugazinova <250272@astanait.edu.kz>
2026-03-27 23:07:46 +0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+   System.out.println("1)");
+    int n1 = sc.nextInt();
+    System.out.println(sum1(n1));
+
+    System.out.println("2)");
+    int n2 = sc.nextInt();
+    int k=sc.nextInt();
+    int arr[]= new int[k];
+    for (int i=0;i<k;i++){
+        arr[i]=sc.nextInt();
+    }
+    System.out.println(sum2(n2,arr));
+
+   System.out.println("3)");
+    int b= sc.nextInt();
+    int n3=sc.nextInt();
+    System.out.println(sum3(b,n3));
+
+    System.out.println("4)");
+    int N=sc.nextInt();
+    reversePrint(N);
+}
+
+public static int sum1(int n){
+    if (n==1) return 1;
+    return n*n+sum1(n-1);
+}
+
+
+public static int sum2(int n,int arr[]){
+    if (n==1) return arr[0];
+    return arr[n-1]+sum2(n-1,arr);
+}
+
+public static int sum3(int b, int n){
+    if (n==0) return 1;
+    return pow(b,n)+sum3(b,n-1);
+}
+private static int pow(int b, int n){
+    if  (n==0) return 1;
+    return b*pow(b,n-1);
+}
+
+public static void reversePrint(int N){
+    Scanner sc = new Scanner(System.in);
+    reverse(N,sc);
+}
+public static void reverse(int N,Scanner sc){
+    if (N==0) return;
+    int n=sc.nextInt();
+    reverse(N-1,sc);
+    System.out.print(n+" ");
 }
